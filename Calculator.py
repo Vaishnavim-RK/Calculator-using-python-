4140fb87b1202c68742cae14be9a99605fbc067e
Vaishnavim-RK <vaishnavim6213@gmail.com>
2025-11-11 23:42:58 +0530

    Create Calculator.py

---
+++ Calculator.py
@@ -0,0 +1,28 @@
+num1_first= input("Enter the first number: ")
+num1 = float(num1_fitst)
+
+# Get the desired operation from the user
+operation = input("Enter the operation (+, -, *, /): ")
+
+# Get the second number from the user
+num2_sec = input("Enter the second number: ")
+num2 = float(num2_sec)
+
+# Perform the calculation
+if operation == '+':
+    result = num1 + num2
+    print(f"{num1} + {num2} = {result}")
+elif operation == '-':
+    result = num1 - num2
+    print(f"{num1} - {num2} = {result}")
+elif operation == '*':
+    result = num1 * num2
+    print(f"{num1} * {num2} = {result}")
+elif operation == '/':
+    if num2 != 0: 
+        result = num1 / num2
+        print(f"{num1} / {num2} = {result}")
+    else:
+        print("Error: Division by zero is not allowed.")
+else:
+    print("Invalid operation. Please use +, -, *, or /.")
